42ae3d4f2847fd46fee28e0e7622e505c4ecbe27
XuanBach1707 <hathogiang231@gmail.com>
2025-12-11 16:51:45 +0700

    fix(deploy) : try deploy for first time

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  // Thêm đoạn này để tự động chuyển trang chủ về login
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false, // Để false để sau này bạn còn sửa lại trang chủ
+      },
+    ];
+  },
 };
 
 export default nextConfig;
